f329f79b3dc7234dbf40fa1c7443ee36b481680e
rosamola77 <alvarort@icloud.com>
2025-11-27 13:57:27 +0100

    Realizado el ejercicio 3 con su correspondiente test (lo único que no se si está bien es el tipo de grafo [SimpleGraph])

--- PI2_2025_2026_Base_ADDA/src/tests/TestEjercicio3.java
+++ PI2_2025_2026_Base_ADDA/src/tests/TestEjercicio3.java
@@ -1,14 +1,36 @@
 package tests;
 
+import ejercicio3.Colaboracion;
+import ejercicio3.Ejercicio3;
+import ejercicio3.Investigador;
+import org.jgrapht.graph.SimpleGraph;
 
-
-
+import us.lsi.graphs.Graphs2;
+import us.lsi.graphs.GraphsReader;
 
 public class TestEjercicio3 {
 
 	public static void main(String[] args) {
-	
+		testsEjercicio3Ap3();
 	}
-	
+
+
+	public static int count = 0;
+	public static void testsEjercicio3Ap3() {
+		
+		String file = "ficheros\\PI2E3_DatosEntrada.txt";
+		SimpleGraph<Investigador, Colaboracion> g = 
+				GraphsReader.newGraph(file,
+						Investigador::ofFormat,
+						Colaboracion::ofFormat,
+						Graphs2::simpleGraph);
+		
+		System.out.println("************************************************************");
+		System.out.println("PI2 - Ejercicio 3 - Apartado C");
+		System.out.println("************************************************************");
+		
+		System.out.println(Ejercicio3.getMapListaColabroradores_E3C(g));
+
+	}
 
 }
