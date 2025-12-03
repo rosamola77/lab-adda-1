77fd595449c05f313c19aa0440b69b5b5ac95c59
rosamola77 <alvarort@icloud.com>
2025-12-03 13:01:55 +0100

    Ejercicio 4 completado y testeado

--- PI2_2025_2026_Base_ADDA/src/ejercicio4/Ejercicio4.java
+++ PI2_2025_2026_Base_ADDA/src/ejercicio4/Ejercicio4.java
@@ -1,24 +1,117 @@
 package ejercicio4;
 
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Set;
 
 import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
+import org.jgrapht.alg.connectivity.ConnectivityInspector;
+import org.jgrapht.alg.shortestpath.DijkstraManyToManyShortestPaths;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.alg.tour.HeldKarpTSP;
+
+import us.lsi.colors.GraphColors;
+import us.lsi.colors.GraphColors.Color;
+import us.lsi.graphs.Graphs2;
 
 
 public class Ejercicio4 {
 
-	public static GraphPath<Interseccion,Calle> getSubgraph_EJ4A(String mIn, String mOut,Graph <Interseccion, Calle> g, String ftest) {
-		return null;
+	public static GraphPath<Interseccion,Calle> getShortestPathBetweenMonument_EJ4A(String startMonument, String endMonument, Graph<Interseccion,Calle> g, String outputFile) {
+		Interseccion a = null;
+		Interseccion b = null;
+		for (Interseccion e : g.vertexSet()) {
+			if (e.getNombre().equals(startMonument)) {
+				a = e;
+			} else if (e.getNombre().equals(endMonument)) {
+				b = e;
+			}
+		}
+		for (Calle e : g.edgeSet()) {
+			g.setEdgeWeight(e, e.getDuracion());
+		}
+		var alg = new DijkstraShortestPath<>(g);
+		var res = alg.getPath(a, b);
+		GraphColors.toDot(g, "ficheros/grafos/" + outputFile,
+				 v-> v.toString(),
+				 e-> String.valueOf(e.getDuracion()),
+				 v -> GraphColors.colorIf(Color.blue, res.getVertexList().contains(v)),
+				 e -> GraphColors.colorIf(Color.green, res.getEdgeList().contains(e)));
+		return res;
 	}
 	
 	public static GraphPath<Interseccion,Calle> getRecorrido_E4B(Graph <Interseccion, Calle> g) {
-        return null;
+		for (Calle e : g.edgeSet()) {
+			g.setEdgeWeight(e, e.getEsfuerzo());
+		}
+		var tsp = new HeldKarpTSP<Interseccion, Calle>();
+		var res = tsp.getTour(g);
+		GraphColors.toDot(g, "ficheros/grafos/EJ4B.gv",
+				 v-> v.toString(),
+				 e-> String.valueOf(e.getEsfuerzo()),
+				 v -> GraphColors.colorIf(Color.blue, res.getVertexList().contains(v)),
+				 e -> GraphColors.colorIf(Color.green, res.getEdgeList().contains(e)));
+        return res;
 	}
 	
 	public static Graph<Interseccion,Calle> getRecorridoMaxRelevante_E4C(Set<Calle> cs,Graph <Interseccion, Calle> g, String ftest) {
-		return null;
+		g.removeAllEdges(cs);
+		var conInspector = new ConnectivityInspector<Interseccion, Calle>(g);
+		var conComp = conInspector.connectedSets();
+		int count = 0;
+		if (conComp.size() == 1) {
+			GraphColors.toDot(g, "ficheros/grafos/" + ftest,
+					 v-> v.toString(),
+					 e-> String.valueOf(e.toString()),
+					 v -> GraphColors.colorIf(Color.blue, true),
+					 e -> GraphColors.colorIf(Color.green, false));
+			return g;
+		} else {
+			for (Set<Interseccion> l : conComp) {
+				boolean hasMonumento = false;
+				for (Interseccion i : l) {
+					if (i.hasMonumento()) {
+						hasMonumento = true;
+					}
+				}
+				if (hasMonumento) {
+					count += 1;
+				}
+			}
+			if (count > 1) {
+				record Tupla(Set<Interseccion> s, Integer relevancia) {
+				}
+				List<Tupla> lista = new ArrayList<>();
+				for (Set<Interseccion> l : conComp) {
+					Integer rel = 0;
+					for (Interseccion i : l) {
+						if (i.hasMonumento()) {
+							rel += i.getRelevancia();
+						}
+					}
+					lista.add(new Tupla(l, rel));
+				}
+				Comparator<Tupla> cmp = Comparator.comparing(e -> e.relevancia);
+				lista.sort(cmp.reversed());
+				GraphColors.toDot(g, "ficheros/grafos/" + ftest,
+						 v-> v.toString(),
+						 e-> String.valueOf(e.toString()),
+						 v -> GraphColors.colorIf(Color.blue, lista.get(0).s.contains(v)),
+						 e -> GraphColors.colorIf(Color.green, false));
+				return g;
+			} else {
+				GraphColors.toDot(g, "ficheros/grafos/" + ftest,
+						 v-> v.toString(),
+						 e-> String.valueOf(e.toString()),
+						 v -> GraphColors.colorIf(Color.blue, v.hasMonumento()),
+						 e -> GraphColors.colorIf(Color.green, false));
+				return g;
+			}
+			
+		}
 	}
 		
 }
--- PI2_2025_2026_Base_ADDA/src/tests/TestEjercicio4.java
+++ PI2_2025_2026_Base_ADDA/src/tests/TestEjercicio4.java
@@ -1,13 +1,61 @@
 package tests;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import ejercicio4.Calle;
+import ejercicio4.Ejercicio4;
+import ejercicio4.Interseccion;
+import us.lsi.graphs.Graphs2;
+import us.lsi.graphs.GraphsReader;
 
 public class TestEjercicio4 {
 
 	public static void main(String[] args) {
-		
-
+		testsEjercicio4ApA();
+		testsEjercicio4ApB();
+		testsEjercicio4ApC();
 	}
 	
+    static String file = "ficheros\\PI2E4_DatosEntrada.txt";
+    static SimpleWeightedGraph<Interseccion, Calle> g =
+            GraphsReader.newGraph(file,
+                    Interseccion::ofFormat,
+                    Calle::ofFormat,
+                    Graphs2::simpleWeightedGraph);
 
 	
+	public static void testsEjercicio4ApA() {
+	    System.out.println("");
+	    System.out.println("PI2 - Ejercicio 4 - Apartado A");
+	    System.out.println("");
+	    var res = Ejercicio4.getShortestPathBetweenMonument_EJ4A("m1", "m7", g, "EJ4A.gv");
+	    System.out.println(res.toString());
+	}
+	
+	public static void testsEjercicio4ApB() {
+	    System.out.println("");
+	    System.out.println("PI2 - Ejercicio 4 - Apartado B");
+	    System.out.println("");
+	    var res = Ejercicio4.getRecorrido_E4B(g);
+	    System.out.println(res.toString());
+	}
+	
+	public static void testsEjercicio4ApC() {
+	    System.out.println("");
+	    System.out.println("PI2 - Ejercicio 4 - Apartado C");
+	    System.out.println("");
+
+	    Set<Calle> cortadas = new HashSet<>();
+	    System.out.println(g.edgeSet());
+	    for (Calle c : g.edgeSet()) {
+	    	if (c.getId() == 0 || c.getId() == 3 || c.getId() == 6 || c.getId() == 5 || c.getId() == 0) {
+	    		cortadas.add(c);
+	    	}
+	    }
+	    var res = Ejercicio4.getRecorridoMaxRelevante_E4C(cortadas, g, "EJ4C.gv");
+	    System.out.println(res.toString());
+	}
 }
